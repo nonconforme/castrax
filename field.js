5d9387b74a57c81fc1ab4f83d7c6136077855b15
oliverbye <oli.github@mail-filter.com>
2018-11-17 15:02:43 +0000

    Throw bullets about

--- field.js
+++ field.js
@@ -5,12 +5,18 @@ class Field extends Phaser.Scene {
 
     preload() {
         this.load.image('TankFR1', 'assets/tank2fr1.png');
+        this.load.image('Bullet', 'assets/Bullet.png');
     }
     create() {
         this.image = this.add.image(400,300,'TankFR1');
 
         this.input.keyboard.on('keyup_D', function (event) {
            this.image.x +=10;
+        }, this);
+
+        this.input.keyboard.on('keyup_P', function () {
+            var pi = this.physics.add.image(this.image.x, this.image.y, 'Bullet')
+            pi.setVelocity(Phaser.Math.RND.integerInRange(-100,100), -300);
         }, this);
     }
 }
